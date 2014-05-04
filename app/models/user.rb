660e68a9bb46123dcd1f766ed446b2cd0375ab62
nxleung <nxleung@gmail.com>
2014-05-03 22:08:11 -0700

    Added reset password feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,13 @@ class User < ActiveRecord::Base
   		Digest::SHA1.hexdigest(token.to_s)
   	end
 
+    def send_password_reset
+      create_remember_token
+      self.password_reset_sent_at = Time.zone.now
+      save(:validate => false)
+      UserMailer.password_reset(self).deliver
+    end
+
   	private
   		def create_remember_token
   			self.remember_token = User.encrypt(User.new_remember_token)
